7e822ec99e4e86b45c60a8a5541f2ef9c1bd1d90
Azam-JM <155773000+Azam-JM@users.noreply.github.com>
2024-03-17 12:02:30 +0000

    Update LeetCode_20_Stack.java

--- Stack/LeetCode_20_Stack.java
+++ Stack/LeetCode_20_Stack.java
@@ -38,3 +38,37 @@ class Solution {
         return st.empty();
     }
 }
+
+// Approach 2
+class Solution2 {
+    public boolean isValid(String s) {
+        // Create a stack to store characters
+        Stack<Character> st = new Stack<Character>();
+
+        // Iterate through each character in the input string
+        for(int i = 0; i < s.length(); i++) {
+            // If the character is an opening parenthesis, push its corresponding closing parenthesis onto the stack
+            if(s.charAt(i) == '{') {
+                st.push('}');
+            } else if(s.charAt(i) == '[') {
+                st.push(']');
+            } else if(s.charAt(i) == '(') {
+                st.push(')');
+            } else {
+                // If the stack is not empty and the current character matches the top of the stack,
+                // pop the top character from the stack (indicating a matched pair)
+                if(!st.empty() && (s.charAt(i) == st.peek())) {
+                    st.pop();
+                } else {
+                    // If the stack is empty or the current character does not match the top of the stack,
+                    // return false (indicating an unmatched closing parenthesis)
+                    return false;
+                }
+            }
+        }
+        // After iterating through the string, if the stack is empty, return true
+        // Otherwise, return false (there are unmatched opening parentheses)
+        return st.empty();    
+    }
+}
+
