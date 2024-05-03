6487e06cf8c6d17d51d8238535cf3f6d687432a0
Azam-JM <155773000+Azam-JM@users.noreply.github.com>
2024-05-03 16:52:47 +0100

    Update LeetCode33_Medium.java

--- SlidingWindow/LeetCode33_Medium.java
+++ SlidingWindow/LeetCode33_Medium.java
@@ -1,24 +1,19 @@
-/*Example 1:
+/*
+Example 1:
 
-Input: nums = [4,5,6,7,0,1,2], target = 0
-Output: 4
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 Example 2:
 
-Input: nums = [4,5,6,7,0,1,2], target = 3
-Output: -1
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
 Example 3:
 
-Input: nums = [1], target = 0
-Output: -1
- 
-
-Constraints:
-
-1 <= nums.length <= 5000
--104 <= nums[i] <= 104
-All values of nums are unique.
-nums is an ascending array that is possibly rotated.
--104 <= target <= 104
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
 */
 
 class Solution {
