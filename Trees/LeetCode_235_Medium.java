29224f5db0722834971d329c5481037219f4f073
Azam-JM <155773000+Azam-JM@users.noreply.github.com>
2024-05-04 12:31:37 +0100

    Create LeetCode_235_Medium.java

---
+++ Trees/LeetCode_235_Medium.java
@@ -0,0 +1,30 @@
+/*Example 1:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+  */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        TreeNode currentNode = root;
+        while(currentNode != null){
+            if(currentNode.val > p.val && currentNode.val > q.val){
+                currentNode = currentNode.left;
+            } else if(currentNode.val < p.val && currentNode.val < q.val){
+                currentNode = currentNode.right;
+            } else {
+                return currentNode;
+            }
+        }
+        return currentNode;
+    }
+}
